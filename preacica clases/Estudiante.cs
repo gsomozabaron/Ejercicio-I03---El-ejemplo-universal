9a3207d14a03e5b06a4604f24b977617d61bf4e7
gsomozabaron <somozagustavo@gmail.com>
2023-09-24 21:28:45 -0300

    Agregar archivos de proyecto.

---
+++ preacica clases/Estudiante.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace preacica_clases
+{
+    internal class Estudiante
+    {
+        private string _nombre;
+        private string _apellido;
+        private string _legajo;
+        private float _notaPrimerParcial;
+        private float _notaSegundoParcial;
+            
+    
+        public Estudiante(string nombre, string apellido, string legajo)
+        {
+            this._nombre = nombre;
+            this._apellido = apellido;
+            this._legajo = legajo;
+        }
+        public static Random random = new Random();
+
+        public void SetNotaPrimerParcial(int nota)
+        {
+            this._notaPrimerParcial = nota;
+        }
+        public void SetNotaSegundoParcial(int nota)
+        {
+            this._notaSegundoParcial = nota;
+        }
+        private float CalcularPromedio()
+        {
+            return (_notaPrimerParcial + _notaSegundoParcial) / 2;
+        }
+        public float CalcularNotaFinal() 
+        {
+            if (_notaPrimerParcial > 3 && _notaSegundoParcial > 3)
+            {
+                return random.Next(6, 11);
+            }
+            else
+            {
+                return -1;
+            }
+        }
+        public void Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine ($"nombre: {_nombre}, apellido: {_apellido}, legajo # {_legajo}");
+            sb.AppendLine($"nota primer parcial {_notaPrimerParcial}, nota segundo parcial {_notaSegundoParcial}");
+            sb.AppendLine($"promedio: {CalcularPromedio()}");
+            if (CalcularNotaFinal() != -1)
+            {
+                sb.AppendLine($"nota final: {CalcularNotaFinal()}");
+            }
+            else 
+            {
+                sb.AppendLine($"alumno desaprovado");
+            }
+            Console.WriteLine(sb.ToString());
+        }
+        /*    Tendrá un constructor estático que inicializará el atributo estático random.
+            Tendrá un constructor de instancia que inicializará los atributos nombre, apellido y legajo.
+            El método setter SetNotaPrimerParcial permitirá cambiar el valor del atributo notaPrimerParcial.
+            El método setter SetNotaSegundoParcial permitirá cambiar el valor del atributo notaSegundoParcial.
+            El método privado CalcularPromedio retornará el promedio de las dos notas.
+            El método CalcularNotaFinal deberá retornar la nota del final con un número aleatorio entre 6 y 10 incluidos 
+            siempre y cuando las notas del primer y segundo parcial sean mayores o iguales a 4, 
+            caso contrario la inicializará con el valor -1.
+
+            El método Mostrar utilizará StringBuilder para armar una cadena de texto con todos los datos de los alumnos:
+            Nombre, apellido y legajo.
+            Nota del primer y segundo parcial.
+            Promedio.
+            Nota final. Se mostrará sólo si el valor es distinto de -1, caso contrario se mostrará la leyenda "Alumno desaprobado".
+
+            Crear tres instancias de la clase Estudiante (tres objetos) en el método Main.
+            Cargar las notas del primer y segundo parcial a todos los alumnos. Dos deberán estar aprobados y uno desaprobado.
+            Mostrar los datos de todos los alumnos.
+
+            */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
---
+++ preacica clases/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace preacica_clases
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Estudiante estudiante1 = new("gustavo", "somoza", "E00001");
+            Estudiante estudiante2 = new("ivana", "somza", "E00002");
+            Estudiante estudiante3 = new("ariel", "somoza", "E00003");
+
+            estudiante1.SetNotaPrimerParcial(5);
+            estudiante1.SetNotaSegundoParcial(7);
+            
+            estudiante2.SetNotaPrimerParcial(4);
+            estudiante2.SetNotaSegundoParcial(4);
+            
+            estudiante3.SetNotaPrimerParcial(8);
+            estudiante3.SetNotaSegundoParcial(1);
+
+            estudiante1.Mostrar();
+            estudiante2.Mostrar();
+            estudiante3.Mostrar();
+
+
+
+
+        }
+        /*  Crear tres instancias de la clase Estudiante (tres objetos) en el método Main.
+            Cargar las notas del primer y segundo parcial a todos los alumnos. Dos deberán estar aprobados y uno desaprobado.
+            Mostrar los datos de todos los alumnos.
+
+            */
+    }
+}
